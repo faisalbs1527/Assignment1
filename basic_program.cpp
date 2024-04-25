8dc5b6f63fb92bfe6fa1421ac6127ea2f05373c4
faisal Ahammed <faisal.ahamed@brainstation-23.com>
2024-04-25 12:16:14 +0600

    calculate factorial feature added

--- basic_program.cpp
+++ basic_program.cpp
@@ -1,6 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+int factorial(int n){
+    int res=1;
+    for(int i=2;i<=n;i++){
+        res=res*i;
+    }
+    return res;
+}
 int main(){
     cout<<"Hello world"<<endl;
+    int n=5;
+    cout<<factorial(n)<<endl;
     return 0;
 }
