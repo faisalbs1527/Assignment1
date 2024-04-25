d6aaf07e8a3782278dc2ab4bd1dc127179ec23a3
faisal Ahammed <faisal.ahamed@brainstation-23.com>
2024-04-25 12:32:45 +0600

    Mod feature added to factorial

--- basic_program.cpp
+++ basic_program.cpp
@@ -1,16 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
+int mod=1e9+7;
 
 int factorial(int n){
     int res=1;
     for(int i=2;i<=n;i++){
-        res=res*i;
+        res=(res*i)%mod;
     }
-    return res;
+    return res%mod;
 }
 int main(){
     cout<<"Hello world"<<endl;
     int n=5;
-    cout<<factorial(n)<<endl;
+    int res=factorial(n);
+    cout<<res<<endl;
     return 0;
 }
